56b5c8df0e2efe47955fbd2d8e74f54ad51bea13
Heidi Santiago <santiagopeach621@gmail.com>
2023-07-18 10:20:56 -0700

    Initial Commit

---
+++ ToDoListDemo/ToDoListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListDemoApp.swift
+//  ToDoListDemo
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
